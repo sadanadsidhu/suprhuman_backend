426623b1dac2968926b4ee8e6ccf5be92094993c
Sadanand Kumar Yadav <sadanandsidhu@gmail.com>
2024-09-05 00:24:15 +0530

    cors policy

--- index.js
+++ index.js
@@ -6,31 +6,35 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Database Connected"))
-  .catch((err) => console.log("Database is not working"));
+  .catch((err) => console.log("Database is not working", err));
 
+// Define CORS options
 const corsOptions = {
-  origin: ["http://88.222.242.108"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: "http://88.222.242.108", // Make sure this matches the origin of your requests
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
+// Use CORS middleware
 app.use(cors(corsOptions));
 
-app.options("*", cors(corsOptions));
-
+// Parse incoming requests with JSON payloads
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+// Define routes
 app.use("/", require("./routes/user.routes"));
 app.use("/", require("./routes/upgrade.routes"));
 app.use("/", require("./routes/restrint.routes"));
 app.use("/", require("./routes/supergrade.routes"));
 
+// Start the server
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
